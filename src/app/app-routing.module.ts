74dcb0344b05eb8b5ba86b4016978c7a3ead8453
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2020-11-02 15:09:33 +0100

    :sparkles: Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-
+  { path: 'contact', component: ContactComponent },
+  { path: '', pathMatch: 'full', component: HomeComponent }, // Default path
 ];
 
 @NgModule({
@@ -13,4 +16,5 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
