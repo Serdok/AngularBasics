a6452bbbfc51d0115ae1b100b772c7061d6dc5f7
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2020-11-02 16:26:35 +0100

    :sparkles: Added users service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { ContactComponent } from './contact/contact.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  apiUrl = 'https://api.github.com/users';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers(maxUsers: number = 10): Observable<object> {
+    return this.httpClient.get(`${this.apiUrl}?per_page=${maxUsers}`);
+  }
+}
